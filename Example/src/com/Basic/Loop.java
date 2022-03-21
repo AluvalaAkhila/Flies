90a10d6c481f3c90039dd607bdd8819f06567d39
lenovo <lenovo@DESKTOP-EV4CPJH>
2022-03-21 16:41:13 +0530

    Loop Program

---
+++ Example/src/com/Basic/Loop.java
@@ -0,0 +1,16 @@
+package com.Basic;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		int a=20,b=20;
+		if(a<b) {
+			System.out.println("b is greater");
+			
+		}
+		else {
+			System.out.println("a is greater");
+		}
+	}
+
+}
